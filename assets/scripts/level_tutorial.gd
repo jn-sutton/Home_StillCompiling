2c17511356b2105afff5b4591700e75646b62a00
Jacqueline Sutton <jacqueline.n.sutton@gmail.com>
2026-03-03 16:11:59 -0700

    Completed hotkey rebind under controls page in the options menu

---
+++ assets/scripts/level_tutorial.gd
@@ -0,0 +1,29 @@
+extends LevelBase
+
+@onready var dirt_mound = $Environment/DirtMound
+@onready var key_item = $Environment/KeyItem
+@onready var gate = $Environment/gate
+
+var has_key = false
+
+func _on_player_sniffed():
+	if is_player_near(dirt_mound):
+		dirt_mound.visible = true
+
+func _on_player_dug():
+	if dirt_mound.visible and is_player_near(dirt_mound):
+		key_item.visible = true
+		dirt_mound.visible = false
+
+func _on_player_interacted():
+	# Pick up key
+	if key_item.visible and is_player_near(key_item):
+		has_key = true
+		key_item.visible = false
+	
+	# Use key on door
+	if has_key and is_player_near(gate):
+		transition_to_next_level()
+
+func transition_to_next_level():
+	get_tree().change_scene_to_file("res://scenes/levels/level_01.tscn")
--- assets/scripts/main_menu.gd
+++ assets/scripts/main_menu.gd
@@ -21,12 +21,12 @@ func _ready():
 	on_exit_options_menu()
 
 #needs a const since it won't take the location in the change scene
-const Level_0 = preload("res://assets/scenes/levels/level_0.tscn")
+const level_tutorial = preload("res://assets/scenes/levels/level_tutorial.tscn")
 
 #right now goes straight to tutorial level, but will change to intro scene
 #when it is created
 func on_start_pressed() -> void:
-	get_tree().change_scene_to_packed(Level_0)
+	get_tree().change_scene_to_packed(level_tutorial)
 
 func on_options_pressed() -> void:
 	margin_container.visible = false
