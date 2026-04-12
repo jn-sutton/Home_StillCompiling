8cde16b321f898d33f7448320eab12b89ac7d20e
Jacqueline Sutton <jacqueline.n.sutton@gmail.com>
2026-04-12 17:22:52 -0600

    Added fade to black after opening scene and before starting game

--- assets/scripts/opening_scene.gd
+++ assets/scripts/opening_scene.gd
@@ -3,13 +3,17 @@ extends Node2D
 @export var dialogue_resource: DialogueResource
 var dialogue_active = false
 
+@onready var animation_player = $CanvasLayer/AnimationPlayer
+
 func _ready():
 	dialogue_active = true
 	DialogueManager.show_example_dialogue_balloon(dialogue_resource, "opening_scene")
 	await DialogueManager.dialogue_ended
 	dialogue_active = false
-	
-	transition_to_next_level()
+
+	await transition_to_next_level()
 
 func transition_to_next_level():
+	animation_player.play("fade_to_black")
+	await animation_player.animation_finished
 	get_tree().change_scene_to_file("res://assets/scenes/levels/level_0.tscn")
